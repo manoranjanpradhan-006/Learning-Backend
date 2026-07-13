973f254c7fe965880139232e8f0199cf0840203e
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-07-13 15:51:09 +0530

    added asyncHandler, ApiError, ApiResponse and added util codes.

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(statusCode, data, Message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.Message = Message
+        this.success = statusCode < 400
+    }
+}
