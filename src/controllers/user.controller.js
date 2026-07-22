07f1c660cfbcf51986eb6e310deeab5478b9e608
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-07-22 15:21:07 +0530

    adding login feature

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -66,4 +66,40 @@ const registerUser = asyncHandler(async (req, res) => {
     )
 })
 
-export { registerUser }
+const loginUser = asyncHandler(async (req, res) => {
+    // req body -> data
+    // username or email
+    // find the user
+    // password check
+    // access and refresh token
+    // send cookie
+
+
+    const { email, userName, password } = req.body
+
+    if (!userName || !email) {
+        throw new ApiError(400, "UserName or Email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{ userName }, { email }]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "user doesnot exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!user) {
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+
+
+
+})
+export {
+    registerUser,
+    loginUser
+}
