aa820c9451dd6f5a98fca117fad1c037696ef250
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-07-23 23:41:00 +0530

    added a basic features

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,19 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken
+        const refreshToken = user.generateRefreshToken
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
+
+
+
 const registerUser = asyncHandler(async (req, res) => {
     const { fullName, email, userName, password } = req.body
 
