7b4f8bcef16e98ee4db12923854ff755185eaffc
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-07-24 23:56:31 +0530

    small bug

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,7 +56,7 @@ userSchema.pre("save", async function () {
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    await bcrypt.compare(password, this.password)
+    return await bcrypt.compare(password, this.password)
 }
 
 userSchema.methods.generateAccessToken = function () {
