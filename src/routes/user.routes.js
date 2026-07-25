2d80b001b9916d6be07e1e1a58f838df6ad17f66
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-07-25 19:44:06 +0530

    resolved an error

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -18,11 +18,10 @@ router.route("/register").post(
     registerUser
 )
 
-
 router.route("/login").post(loginUser)
 
-
 router.route("/logout").post(verifyJWT, logoutUser)
+
 router.route("/refresh-token").post(refreshAccessToken)
 
 
