e65959337d3b6b7e85f6dac1b1116733e79c4eda
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-07-25 19:28:58 +0530

    added a try catch to not get error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -176,39 +176,43 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
         throw new ApiError(401, "Unauthorized request")
     }
 
-    const decodedToken = jwt.verify(
-        incomingRefreshToken,
-        process.env.REFRESH_TOKEN_SECRET
-    )
-
-    const user = await User.findById?._id
-
-    if (!user) {
-        throw new ApiError(401, "Invalid refresh token.")
-    }
-
-    if (incomingRefreshToken !== user?.refreshToken) {
-        throw new ApiError(401, "Refresh token is expired or used")
-    }
-
-    const options = {
-        httpOnly: true,
-        secure: true
-    }
-
-    const { accessToken, newRefreshToken } = await generateAccessAndRefreshTokens(user._id)
-
-    return res
-        .status(200)
-        .cookie("accessToken", accessToken, options)
-        .cookie("refreshToken", newRefreshToken, options)
-        .json(
-            new ApiResponse(
-                200,
-                { accessToken, newRefreshToken },
-                "Access token refreshed successfully"
-            )
+    try {
+        const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
         )
+
+        const user = await User.findById?._id
+
+        if (!user) {
+            throw new ApiError(401, "Invalid refresh token.")
+        }
+
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError(401, "Refresh token is expired or used")
+        }
+
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+
+        const { accessToken, newRefreshToken } = await generateAccessAndRefreshTokens(user._id)
+
+        return res
+            .status(200)
+            .cookie("accessToken", accessToken, options)
+            .cookie("refreshToken", newRefreshToken, options)
+            .json(
+                new ApiResponse(
+                    200,
+                    { accessToken, newRefreshToken },
+                    "Access token refreshed successfully"
+                )
+            )
+    } catch (error) {
+        throw new ApiError(401)
+    }
 
 })
 
