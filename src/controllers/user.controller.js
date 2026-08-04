9dc20bd50c603aeea1a3e15f0075a502d1621458
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-04 21:04:24 +0530

    fix: wrap response in ApiResponse for getCurrentUser function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -238,7 +238,13 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res
         .status(200)
-        .json(200, req.user, "Current user fetched successfully")
+        .json(
+            new ApiResponse(
+                200,
+                req.user,
+                "Current user fetched successfully"
+            )
+        )
 })
 
 const updateUserDetails = asyncHandler(async (req, res) => {
