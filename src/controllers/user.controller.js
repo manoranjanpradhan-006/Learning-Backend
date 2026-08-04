2b8948cfcc8748f67e63c2fef06e4e3c392f858f
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-04 21:32:13 +0530

    fix: format ApiResponse instantiation in updateUserCoverImage function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -343,7 +343,12 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     return res
         .status(200)
         .json(
-            new ApiResponse(200, user, "Cover image updated successfully")
+            new ApiResponse
+                (
+                    200,
+                    user,
+                    "Cover image updated successfully"
+                )
         )
 })
 
