b0a3ae67bf2b5edc6639f7851310b66f21c1415a
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-06 00:15:12 +0530

    feat: add $project stage to getUserChannelProfile aggregation for user details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -383,6 +383,18 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
                     }
                 }
             }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
         }
     ])
 })
